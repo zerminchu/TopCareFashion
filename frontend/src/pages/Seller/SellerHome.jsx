01bd78a920d8827e3f56a3c0eb3c4cfb34c3bd42
Zer Min <zermin55@gmail.com>
2023-08-29 03:33:42 +0800

    Update SellerHome.jsx

--- frontend/src/pages/Seller/SellerHome.jsx
+++ frontend/src/pages/Seller/SellerHome.jsx
@@ -1,51 +1,134 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Card, Text } from "@mantine/core";
+import {
+  Card,
+  Image,
+  Text,
+  Group,
+  Badge,
+  createStyles,
+  Center,
+  Button,
+  rem,
+} from "@mantine/core";
 import axios from "axios";
 
-// Import your useStyles function here if you have it
+const useStyles = createStyles((theme) => ({
+  card: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+    marginBottom: theme.spacing.md,
+  },
+
+  imageSection: {
+    padding: theme.spacing.md,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  label: {
+    marginBottom: theme.spacing.xs,
+    lineHeight: 1,
+    fontWeight: 700,
+    fontSize: theme.fontSizes.xs,
+    letterSpacing: rem(-0.25),
+    textTransform: "uppercase",
+  },
+
+  section: {
+    padding: theme.spacing.md,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  icon: {
+    marginRight: rem(5),
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[2]
+        : theme.colors.gray[5],
+  },
+}));
 
 function SellerCards() {
   const { id } = useParams();
-  // const { classes } = useStyles();
   const [category, setCategory] = useState("");
   const [condition, setCondition] = useState("");
-  const [products, setProducts] = useState([]);
+  const [colour, setColour] = useState("");
+  const [image, setImage] = useState("");
+  const { classes } = useStyles();
+  const [items, setItems] = useState([]);
 
-  // Fetch products when the component mounts
   useEffect(() => {
-    axios
-      .get(`http://localhost:8000/view-item/${id}/`)
-      .then((response) => {
-        setProducts(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-      });
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/view-item/${id}/`
+        );
+        const fetchedItems = response.data; // Array of items
+        setItems(fetchedItems);
+      } catch (error) {
+        console.error("Error fetching Items:", error);
+      }
+    };
+
+    fetchData();
   }, [id]);
 
-  // Apply filtering when category or condition changes
-  useEffect(() => {
-    const filteredProducts = products.filter((product) => {
-      if (
-        (category && product.category !== category) ||
-        (condition && product.condition !== condition)
-      ) {
-        return false;
-      }
-      return true;
-    });
-
-    setProducts(filteredProducts);
-  }, [category, condition, products]);
-
   return (
-    <div>
-      {products.map((product) => (
-        <Card key={product.id}>
-          <Text>{product.category}</Text>
-          <Text>{product.condition}</Text>
-          {/* Render other product information here */}
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {items.map((item, index) => (
+        <Card
+          withBorder
+          radius="md"
+          className={classes.card}
+          key={index}
+          style={{
+            flex: "0 0 calc(25% - 1rem)",
+            margin: "0.5rem",
+            padding: "0.5rem",
+          }}
+        >
+          <Card.Section className={classes.imageSection}>
+            <Image
+              src={item.image_urls[0]}
+              alt={item.category}
+              width={150}
+              height={180}
+            />
+          </Card.Section>
+
+          <Group position="apart" mt="md">
+            <div>
+              <Text>{item.condition}</Text>
+            </div>
+          </Group>
+
+          <Card.Section className={classes.section} mt="md">
+            <Text fz="sm" c="dimmed" className={classes.label}>
+              {item.category}
+            </Text>
+
+            <Group spacing={8} mb={-8}>
+              {/* Add your features here */}
+            </Group>
+          </Card.Section>
+
+          <Card.Section className={classes.section}>
+            <Group spacing={30}>
+              <div>
+                <Text fz="xl" fw={700} sx={{ lineHeight: 1 }}>
+                  $168.00
+                </Text>
+              </div>
+            </Group>
+          </Card.Section>
         </Card>
       ))}
     </div>
