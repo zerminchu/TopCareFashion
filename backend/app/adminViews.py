1d86b675a859bdfd7c4bc79162e134b5147e0157
Alviandocendrasa <alviandocendrasa@gmail.com>
2023-11-10 17:08:02 +0800

    Initial buyer admin

---
+++ backend/app/adminViews.py
@@ -0,0 +1,176 @@
+import json
+from datetime import datetime
+
+import stripe
+from config.firebase import firebase
+from django.core.files.base import ContentFile
+from django.core.files.storage import FileSystemStorage
+from django.core.validators import validate_email
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from firebase_admin import auth, firestore, storage
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .exceptions import *
+from .models import *
+from .serializer import *
+
+@api_view(["GET", "POST"])
+def allUsers(request):
+  if request.method == "GET":
+    try:
+        db = firestore.client()
+
+        role = request.GET.get('role', None)
+        print("role: ", role)
+
+        if(role is not None):
+           userRef = db.collection("Users").where("role", "==", role)
+           userData = userRef.get()
+           response = [user.to_dict() for user in userData]
+           
+           return JsonResponse({
+              'status': "success",
+              'message': "All user data retrieved successfully",
+              'data': response
+            }, status=200)
+
+        userRef = db.collection("Users")
+        userData = userRef.get()
+
+        response = [user.to_dict() for user in userData]
+        
+        return JsonResponse({
+          'status': "success",
+          'message': "All user data retrieved successfully",
+          'data': response
+        }, status=200)
+    
+    except Exception as e:
+      return JsonResponse({
+          "status": "error",
+          "message": str(e)
+      }, status=400)
+
+  elif request.method == "POST":
+      try:
+        # Initialize firebase variable
+        firebaseStorage = firebase.storage()
+        db = firestore.client()
+
+        data = request.data
+
+        user_id = data.get('user_id')
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        profile_image = data.get('profile_image')
+        gender = data.get('gender')
+
+        # Buyer
+        phone_number = data.get('phone_number')
+        preferences_condition = data.get('preferences_condition')
+        preferences_gender = data.get('preferences_gender')
+        preferences_price_range = data.get('preferences_price_range')
+        preferences_size = data.get('preferences_size')
+
+        userRef = db.collection("Users").document(user_id)
+        userData = (userRef.get()).to_dict()
+
+        if (userData and userData["role"] == "buyer"):
+            serializer = BuyerUpdateProfileSerializer(data=data)
+        elif (userData and userData["role"] == "seller"):
+            serializer = SellerUpdateProfileSerializer(data=data)
+
+        if (serializer.is_valid()):
+            updatedData = {}
+
+
+            # User want to update profile image
+            if (profile_image):
+                # Validate file format
+                allowed_formats = ['image/jpeg', 'image/jpg', 'image/png']
+                if profile_image.content_type not in allowed_formats:
+                    raise Exception(
+                        "Invalid file format. Only PNG, JPEG, and JPG formats are allowed.")
+
+                # Upload the file to Firebase Storage
+                firebaseStorage.child(
+                    f"UserProfile/{user_id}").put(profile_image)
+
+                # Generate public image url
+                profile_image = firebaseStorage.child(
+                    f"UserProfile/{user_id}").get_url(None)
+
+                # Data to be updated for user
+                updatedData = {
+                    "name.first_name": first_name,
+                    "name.last_name": last_name,
+                    "profile_image_url": profile_image,
+                    "gender": gender
+                }
+
+            # User does not want to update profile image
+            else:
+                # Update data into firestore
+                collectionRef = db.collection('Users').document(user_id)
+
+                # Data to be updated for user
+                updatedData = {
+                    "name.first_name": first_name,
+                    "name.last_name": last_name,
+                    "gender": gender
+                }
+
+            # Check whether it has phone number data or not
+            if (phone_number and preferences_condition and preferences_gender and preferences_price_range and preferences_size):
+                updatedData["phone_number"] = phone_number
+                updatedData["preferences.condition"] = preferences_condition
+                updatedData["preferences.gender"] = preferences_gender
+                updatedData["preferences.price"]= json.loads(preferences_price_range)
+                updatedData["preferences.size"] = preferences_size
+
+
+            # Update data into firestore
+            collectionRef = db.collection('Users').document(user_id)
+            collectionRef.update(updatedData)
+
+        else:
+            raise Exception(serializer.errors)
+
+        return JsonResponse({
+            'status': "success",
+            'message': "User profile updated successfully",
+            'data': {
+                "updatedFirstName": first_name,
+                "updatedLastName": last_name,
+                "updatedProfileImage": profile_image,
+                "updatedGender": gender,
+                "updatedPhoneNumber": phone_number
+            }
+        }, status=200)
+
+      except Exception as e:
+          return JsonResponse({
+              "status": "error",
+              "message": str(e)
+          }, status=400)
+
+@api_view(["GET"])
+def oneUser(request, user_id):
+    if request.method == "GET":
+        try:
+            data = request.data
+            print("params: ", user_id)
+
+            return JsonResponse({
+                'status': "success",
+                'message': "User session data is retrieved successfully",
+            }, status=200)
+
+        except Exception as e:
+            return JsonResponse({
+                "status": "error",
+                "message": str(e)
+            }, status=400)
---
+++ frontend/src/components/Admin/AdminBuyerItem.jsx
@@ -0,0 +1,226 @@
+import React, { useRef } from "react";
+import { useForm } from "@mantine/form";
+import { Button, Group, Image, Radio, Select, TextInput } from "@mantine/core";
+import IlDefaultAvatar from "../../assets/illustrations/il_avatar.png";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { showNotifications } from "../../utils/ShowNotification";
+
+function AdminBuyerItem(props) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fileInputRef = useRef(null);
+
+  const form = useForm({
+    initialValues: {
+      userId: props.data.user_id || "",
+      firstName: props.data.name.first_name || "",
+      lastName: props.data.name.last_name || "",
+      gender: props.data.gender || "",
+      email: props.data.email || "",
+      dateOfBirth: props.data.date_of_birth || "",
+      showImage: props.data.profile_image_url || "",
+      profileImage: "",
+      phoneNumber: props.data.phone_number || "",
+      preferencesGender: props.data.preferences.gender || "",
+      preferencesPriceRange: JSON.stringify(props.data.preferences.price) || "",
+      preferencesCondition: props.data.preferences.condition || "",
+      preferencesSize: props.data.preferences.size || "",
+    },
+  });
+
+  const updateOnClick = async () => {
+    console.log("data ot pass server: ", form.values);
+
+    try {
+      dispatch({ type: "SET_LOADING", value: true });
+
+      const formData = new FormData();
+
+      formData.append("user_id", form.values.userId);
+      formData.append("first_name", form.values.firstName);
+      formData.append("last_name", form.values.lastName);
+      formData.append("gender", form.values.gender);
+      formData.append("profile_image", form.values.profileImage);
+      formData.append("phone_number", form.values.phoneNumber);
+      formData.append(
+        "preferences_condition",
+        form.values.preferencesCondition
+      );
+      formData.append("preferences_gender", form.values.preferencesGender);
+      formData.append(
+        "preferences_price_range",
+        form.values.preferencesPriceRange
+      );
+      formData.append("preferences_size", form.values.preferencesSize);
+
+      const url =
+        import.meta.env.VITE_NODE_ENV == "DEV"
+          ? import.meta.env.VITE_API_DEV
+          : import.meta.env.VITE_API_PROD;
+
+      const response = await axios.post(`${url}/admin/users/`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      dispatch({ type: "SET_LOADING", value: false });
+
+      navigate("/");
+
+      showNotifications({
+        status: response.data.status,
+        title: "Success",
+        message: response.data.message,
+      });
+    } catch (error) {
+      dispatch({ type: "SET_LOADING", value: false });
+
+      showNotifications({
+        status: "error",
+        title: "Error",
+        message: error.response.data.message,
+      });
+    }
+  };
+
+  const imageOnClickHandler = () => {
+    fileInputRef.current.click();
+  };
+
+  const imageChangeHandler = (event) => {
+    const file = event.target.files[0];
+    form.setValues({ profileImage: file });
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        form.setValues({ showImage: reader.result });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <tr>
+      <td>
+        <Image
+          width={35}
+          height={35}
+          radius="50% "
+          src={form.values.showImage || IlDefaultAvatar}
+          onClick={imageOnClickHandler}
+          alt="Selected"
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={imageChangeHandler}
+          ref={fileInputRef}
+          style={{ display: "none" }}
+        />
+      </td>
+      <td>{props.data.user_id}</td>
+      <td>
+        <TextInput
+          disabled
+          placeholder="Email"
+          {...form.getInputProps("email")}
+        />
+      </td>
+      <td>
+        <TextInput
+          placeholder="First Name"
+          {...form.getInputProps("firstName")}
+        />
+      </td>
+      <td>
+        <TextInput
+          placeholder="Last Name"
+          {...form.getInputProps("lastName")}
+        />
+      </td>
+      <td>
+        <TextInput
+          placeholder="Phone Number"
+          {...form.getInputProps("phoneNumber")}
+        />
+      </td>
+      <td>
+        <Select
+          withAsterisk
+          data={[
+            { value: "male", label: "Male" },
+            { value: "female", label: "Female" },
+          ]}
+          value={form.values.gender}
+          onChange={(value) => form.setValues({ gender: value })}
+        />
+      </td>
+      <td>
+        <Select
+          withAsterisk
+          data={[
+            { value: "men", label: "Men" },
+            { value: "women", label: "Women" },
+          ]}
+          value={form.values.preferencesGender}
+          onChange={(value) => form.setValues({ preferencesGender: value })}
+        />
+      </td>
+      <td>
+        <Select
+          withAsterisk
+          data={[
+            { value: "Brand New", label: "Brand New" },
+            { value: "Lightly Used", label: "Lightly Used" },
+            { value: "Well Used", label: "Well Used" },
+          ]}
+          value={form.values.preferencesCondition}
+          onChange={(value) => form.setValues({ preferencesCondition: value })}
+        />
+      </td>
+      <td>
+        <Select
+          withAsterisk
+          data={[
+            { value: "XS", label: "XS" },
+            { value: "S", label: "S" },
+            { value: "M", label: "M" },
+            { value: "L", label: "L" },
+            { value: "XL", label: "XL" },
+            { value: "XXL", label: "XXL" },
+            { value: "Free Size", label: "Free Size" },
+          ]}
+          value={form.values.preferencesSize}
+          onChange={(value) => form.setValues({ preferencesSize: value })}
+        />
+      </td>
+      <td>
+        <Select
+          withAsterisk
+          data={[
+            { value: '["all price"]', label: "All Price" },
+            { value: "[10]", label: "Below $10" },
+            { value: "[10,50]", label: "$10 - $50" },
+            { value: "[50,100]", label: "$50 - $100" },
+            { value: "[100,200]", label: "$100 - $200" },
+            { value: "[200]", label: "Above  $200" },
+          ]}
+          value={form.values.preferencesPriceRange}
+          onChange={(value) => form.setValues({ preferencesPriceRange: value })}
+        />
+      </td>
+      <td>
+        <Button onClick={updateOnClick} color="blue">
+          Update
+        </Button>
+        <Button color="red">Suspend</Button>
+      </td>
+    </tr>
+  );
+}
+
+export default AdminBuyerItem;
