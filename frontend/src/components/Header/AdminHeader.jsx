09d0060bae78b09889f1b4c895b9c375ac557c73
Alviandocendrasa <alviandocendrasa@gmail.com>
2023-11-10 11:56:09 +0800

    Implemented initial admin page

--- frontend/src/components/Header/AdminHeader.jsx
+++ frontend/src/components/Header/AdminHeader.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import classes from "./SellerHeader.module.css";
+import ILLogo from "../../assets/illustrations/il_logo.png";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 
-function AdminHeader() {
+import { Button, Text } from "@mantine/core";
+
+function AdminHeader(props) {
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate("/");
+    window.location.reload();
+  };
+
+  const logoutOnClick = () => {
+    navigate("/");
+    Cookies.remove("firebaseIdToken");
+    Cookies.remove("userRole");
+    window.location.reload();
+  };
+
   return (
-    <div className="header">
-      <span className="business-name">Top Care Fashion Admin</span>
+    <div className={classes.container}>
+      <div className={classes.leftside}>
+        <Link onClick={handleLogoClick}>
+          <img src={ILLogo} width={50} height={50} />
+        </Link>
+        <Text fw={700} fz="xl">
+          Top Care Fashion
+        </Text>
+      </div>
+      <Button onClick={logoutOnClick}>Log out</Button>
     </div>
   );
 }
